81196161bb3986c8622128b24e3fe6ad185db3e4
Javjy6 <Javjy6@gmail.com>
2024-10-14 20:00:41 +0200

    E6

--- src/App.js
+++ src/App.js
@@ -51,6 +51,23 @@ const estils = StyleSheet.create({
   imatge: {
     width: 200,
     height: 200,
+  },
+  upv:{
+    backgroundColor: 'purple',
+    fontSize: 10,
+    fontWeight: '600',
+    padding: 4,
+    paddingLeft: 12,
+    textAlign: 'left',
+    color: 'grey',
+  },
+  florida: {
+    backgroundColor: 'red',
+    fontSize: 12,
+    fontWeight: '600',
+    padding: 4,
+    paddingRight: 12,
+    textAlign: 'right',
   }
 });
 
